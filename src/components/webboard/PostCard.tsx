a6bea9618a4022ebf91cf17d1987eff45988c833
manto123123123 <lhw971103@gmail.com>
2025-06-07 23:24:48 +0900

    ✨ :Feat 게시물 카드 퍼블리싱

---
+++ src/components/webboard/PostCard.tsx
@@ -0,0 +1,51 @@
+import image from "../../assets/images/KakaoTalk_20241210_084712919.jpg";
+
+export default function Postcard() {
+  return (
+    <div
+      className="flex items-stretch w-[1080px] h-[250px]
+                 border-2 rounded-xl border-[#FFC7ED]
+                 hover:border-[#FF66B3] hover:shadow-lg
+                 transition-all duration-200
+                 bg-white cursor-pointer"
+    >
+      <div className="flex-1 p-4 flex flex-col">
+        <div className="space-y-2">
+          {/* 프로필 */}
+          <div className="flex items-center">
+            <img
+              src={image}
+              alt="사진"
+              className="w-[30px] h-[30px] rounded-full object-cover mr-2"
+            />
+            <span className="font-medium">이현우</span>
+            <span className="text-[#969696] ml-2">· 2025-06-07</span>
+          </div>
+
+          {/* 제목 */}
+          <h2 className="text-lg font-semibold">안녕하세요 이현우에요</h2>
+
+          {/* 본문 */}
+          <p className="leading-relaxed overflow-hidden line-clamp-4">제가 진짜 좀 힘들어요</p>
+        </div>
+
+        {/* 좋아요 댓글 */}
+        <div className="flex items-center space-x-6 mt-auto">
+          <div className="flex items-center">
+            <span className="heart mr-1" />
+            <span>10</span>
+          </div>
+          <div className="flex items-center">
+            <span className="comment mr-1" />
+            <span>71</span>
+          </div>
+        </div>
+      </div>
+
+      {/* 우측 이미지 */}
+      <div className="p-4 flex-shrink-0 self-center">
+        <img src={image} alt="Post" className="w-[144px] h-[144px] object-cover rounded-lg" />
+      </div>
+    </div>
+  );
+}
