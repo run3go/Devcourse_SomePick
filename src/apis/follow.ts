590734a344cad5e8b581ffb69cf06449c1c0c86f
run3go <qkrwjdtn09@gmail.com>
2025-06-08 21:24:15 +0900

    ✏️ Fix: 팔로잉, 팔로우 유저목록 분리

--- src/apis/follow.ts
+++ src/apis/follow.ts
@@ -1,6 +1,6 @@
 import supabase from "../utils/supabase";
-// 유저 팔로우 목록 가져오기 ()
-export const fetchFollowList = async () => {
+// 팔로잉하는 유저 목록 가져오기 ()
+export const fetchFollowingList = async () => {
   try {
     const {
       data: { session },
@@ -19,27 +19,38 @@ export const fetchFollowList = async () => {
       )
       .eq("user_id", session.user.id);
     if (followingError) {
-      console.log("팔로우 실패:", followingError.message);
+      console.log("팔로잉 가져오기 실패:", followingError.message);
       return;
     }
+    return followings;
+  } catch (e) {
+    console.error(e);
+  }
+};
+// 나를 팔로우하는 팔로워 유저 목록 가져오기 ()
+export const fetchFollowerList = async () => {
+  try {
+    const {
+      data: { session },
+    } = await supabase.auth.getSession();
+    if (!session) return;
     const { data: followers, error: followerError } = await supabase
       .from("follows")
       .select(
         `
-        follower:follow_id(
-            id,
-            nickname,
-            main_image
+      follower:follow_id(
+        id,
+        nickname,
+        main_image
         )
         `
       )
       .eq("follow_id", session.user.id);
     if (followerError) {
-      console.log("팔로우 실패:", followerError.message);
+      console.log("팔로워 가져오기 실패:", followerError.message);
       return;
     }
-    console.log("팔로우 성공");
-    return { followings, followers };
+    return followers;
   } catch (e) {
     console.error(e);
   }
