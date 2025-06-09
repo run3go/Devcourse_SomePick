15b52ae209c3fd0ea161ddfd3de23707ce077ce7
JINJIY23 <dhkswksla22@naver.com>
2025-06-09 12:15:04 +0900

    💄 레이아웃 랜더링
    레이아웃, 운세 랜더링 스타일 수정

--- src/routes/pages/TodayFortunePage.tsx
+++ src/routes/pages/TodayFortunePage.tsx
@@ -1,7 +1,13 @@
+import FortuneCards from "../../components/fortune/FortuneCards";
+import FortuneInfo from "../../components/fortune/FortuneInfo";
+import ShareButton from "../../components/fortune/ShareButton";
+
 export default function TodayFortunePage() {
   return (
     <>
-      <h1>TodayFortunePage Component</h1>
+      <FortuneInfo />
+      <FortuneCards />
+      <ShareButton />
     </>
   );
 }
