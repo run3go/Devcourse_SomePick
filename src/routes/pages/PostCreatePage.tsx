397071016335febc7b4553b235bf7d0df9bfa43e
YooJung Kwon <best106yj@naver.com>
2025-06-09 23:31:52 +0900

    🎨 Style: postCreate 디자인 피드백 반영

--- src/routes/pages/PostCreatePage.tsx
+++ src/routes/pages/PostCreatePage.tsx
@@ -20,24 +20,26 @@ export default function PostCreatePage() {
             </p> */}
           </div>
 
-          <div className="flex flex-col w-[1080px] [height:calc(100vh-340px)] border border-[var(--primary-pink)] rounded-2xl px-12.5 py-7.5">
-            <div className="border-b border-[var(--gray-700)] w-full pb-5 mb-5">
-              <label className="text-xl">
-                제목
-                <input
-                  type="text"
-                  placeholder="제목을 작성해주세요"
-                  className="ml-7 placeholder:text-[var(--gray-500)] focus:outline-none"
-                />
-              </label>
+          <div className="flex flex-col w-[1080px] [height:calc(100vh-340px)] border-t-2 border-[var(--gray-500)] pl-2 py-7.5">
+            <label className="w-full mb-5">
+              제목
+              <input
+                type="text"
+                placeholder="제목을 작성해주세요"
+                className="ml-5 placeholder:text-[var(--gray-500)] focus:outline-none bg-[var(--gray-100)] p-2 w-[94%] rounded-lg"
+              />
+            </label>
+
+            <div className="w-full mb-5 flex">
+              <label htmlFor="content">내용</label>
+              <textarea
+                id="content"
+                placeholder="당신의 이야기를 자유롭게 들려주세요"
+                className="placeholder:text-[var(--gray-500)] ml-5 h-[300px] w-[94%] focus:outline-none resize-none bg-[var(--gray-100)] rounded-lg p-2"
+              />
             </div>
 
-            <textarea
-              placeholder="당신의 이야기를 자유롭게 들려주세요"
-              className="placeholder:text-[var(--gray-500)] h-[487px] focus:outline-none resize-none"
-            />
-
-            <label className="flex justify-center items-center size-18 bg-[var(--primary-pink)] rounded-2xl cursor-pointer hover:bg-[var(--primary-pink-tone)]">
+            <label className="flex justify-center ml-13 items-center size-18 bg-[var(--primary-pink)] rounded-2xl cursor-pointer hover:bg-[var(--primary-pink-tone)]">
               <Icon width="16px" height="16px" left="-323px" top="-762px" />
               <input type="file" accept="image/*" className="hidden" />
             </label>
