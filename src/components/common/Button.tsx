7893f49ddfea1e48a3e0b6e838b1228f7abc59f3
manto123123123 <lhw971103@gmail.com>
2025-06-09 09:02:41 +0900

    ♻️ :Refactor 버튼 아이콘 props 받게 수정

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 
 interface ButtonProps {
+  text?: string;
   width?: string; // Tailwind width 클래스 (예: "w-[70px]")
   height?: string; // Tailwind height 클래스 (예: "h-[25px]")
   borderRadius?: string; // ex. "8px", "12px"
   children?: React.ReactNode;
+  icon?: React.ReactNode;
   onClick?: () => void;
 }
 
@@ -14,6 +16,8 @@ export default function Button({
   borderRadius = "4px",
   children = "",
   onClick,
+  text = "text-black text-sm font-medium",
+  icon,
 }: ButtonProps) {
   const style = { borderRadius };
 
@@ -23,13 +27,14 @@ export default function Button({
       style={style}
       className={`
         ${width} ${height}
+        ${text}
         bg-[#FFC7ED]
-        text-black text-sm font-medium
         hover:bg-[#FF9fd6]
         transition-colors duration-150
         cursor-pointer
       `}
     >
+      {icon && <span className="mr-2">{icon}</span>}
       {children}
     </button>
   );
