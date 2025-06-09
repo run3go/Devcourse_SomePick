0d7e5c9dc06c95113c747db54b411607e1576042
hansanga <cab08132@naver.com>
2025-06-09 16:46:29 +0900

    .

--- src/components/post/MoreMenu.tsx
+++ src/components/post/MoreMenu.tsx
@@ -1,30 +1,32 @@
-import Imoji from '../../assets/images/spriteImage.png';
+import Imoji from "../../assets/images/spriteImage.png";
 
 export default function MoreMenu() {
   return (
     <>
-      <div className='absolute right-0 mt-2 bg-white p-2.5 rounded-[10px] border flex flex-col w-[82px] z-50'>
-        <div className='flex items-center gap-[5px] pb-2.5'>
-          <button className='text-[12px] cursor-pointer'>수정하기</button>
+      <div className="absolute right-0 mt-2 bg-white p-2.5 rounded-[10px] border border-[var(--gray-500)] flex flex-col w-[82px] z-50">
+        <div className="flex items-center justify-center gap-[5px] pb-2.5">
+          <button className="text-[10px] text-[var(--gray-700)] cursor-pointer">
+            수정하기
+          </button>
           <a
-            className='inline-block w-[12px] h-[12px] cursor-pointer'
+            className="inline-block w-[12px] h-[12px] cursor-pointer"
             style={{
               backgroundImage: `url(${Imoji})`,
-              backgroundRepeat: 'no-repeat',
-              backgroundPosition: '-556px -764px',
+              backgroundRepeat: "no-repeat",
+              backgroundPosition: "-556px -764px",
             }}
           ></a>
         </div>
-        <div className='flex items-center gap-[5px]'>
-          <button className='text-[#E94235] text-[12px] cursor-pointer'>
+        <div className="flex items-center justify-center gap-[5px]">
+          <button className="text-[var(--red-60)] text-[10px] cursor-pointer">
             삭제하기
           </button>
           <a
-            className='inline-block w-[12px] h-[13px] cursor-pointer'
+            className="inline-block w-[12px] h-[13px] cursor-pointer"
             style={{
               backgroundImage: `url(${Imoji})`,
-              backgroundRepeat: 'no-repeat',
-              backgroundPosition: '-597px -763px',
+              backgroundRepeat: "no-repeat",
+              backgroundPosition: "-597px -763px",
             }}
           ></a>
         </div>
