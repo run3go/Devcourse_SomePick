af2a46019a4b59a07622f0ee2913ebd0d93b5c31
JINJIY23 <dhkswksla22@naver.com>
2025-06-11 15:36:03 +0900

    🎨 다크모드 css 파일 수정 및 메인 다크모드 적용
    다크모드 적용

--- src/components/modals/HeaderModal.tsx
+++ src/components/modals/HeaderModal.tsx
@@ -13,7 +13,6 @@ export default function HeaderModal({ onClose }: HeaderModalPropds) {
 
   const setLogout = useAuthStore((state) => state.setLogout);
   const session = useAuthStore<Session | null>((state) => state.session);
-  // console.log("안녕", session?.user.id);
 
   const navigate = useNavigate();
 
@@ -32,12 +31,12 @@ export default function HeaderModal({ onClose }: HeaderModalPropds) {
   };
   return (
     <>
-      <ul className="flex flex-col text-[var(--gray-50)] absolute top-[50px] w-[130px] h-[104px] border border-[var(--primary-pink)] rounded-[20px] bg-white dark:bg-black">
+      <ul className="flex flex-col text-[var(--gray-50)] dark:text-[var(--dark-gray-700)] absolute top-[50px] w-[130px] h-[104px] border border-[var(--primary-pink)] rounded-[20px] bg-[var(--white)] dark:bg-[var(--dark-bg-primary)]">
         <li
           onClick={handleMyProfile}
           className={twMerge(
             "basis-1/3 flex justify-center items-center border-b border-[var(--primary-pink)]",
-            "rounded-t-[20px] hover:text-black cursor-pointer hover:shadow-[inset_0_0_3px_rgba(0,0,0,0.5)] hover:shadow-(color:--primary-pink-tone)"
+            "rounded-t-[20px] hover:text-black dark:hover:text-[var(--dark-gray-100)] cursor-pointer hover:shadow-[inset_0_0_3px_rgba(0,0,0,0.5)] hover:shadow-(color:--primary-pink-tone)"
           )}
         >
           마이 페이지
@@ -46,7 +45,7 @@ export default function HeaderModal({ onClose }: HeaderModalPropds) {
           onClick={handleLogout}
           className={twMerge(
             "basis-1/3 flex justify-center items-center border-b border-[var(--primary-pink)]",
-            "hover:text-black cursor-pointer hover:shadow-[inset_0_0_3px_rgba(0,0,0,0.5)] hover:shadow-(color:--primary-pink-tone)"
+            "hover:text-black cursor-pointer dark:hover:text-[var(--dark-gray-100)] hover:shadow-[inset_0_0_3px_rgba(0,0,0,0.5)] hover:shadow-(color:--primary-pink-tone)"
           )}
         >
           로그아웃
@@ -54,7 +53,7 @@ export default function HeaderModal({ onClose }: HeaderModalPropds) {
         <li
           className={twMerge(
             "basis-1/3 flex justify-center items-center",
-            "rounded-b-[20px] hover:text-black cursor-pointer hover:shadow-[inset_0_0_3px_rgba(0,0,0,0.5)] hover:shadow-(color:--primary-pink-tone)"
+            "rounded-b-[20px] hover:text-black dark:hover:text-[var(--dark-gray-100)] cursor-pointer hover:shadow-[inset_0_0_3px_rgba(0,0,0,0.5)] hover:shadow-(color:--primary-pink-tone)"
           )}
         >
           <button onClick={toggleDarkMode}>
