621ba3b341467078441a1f87068548be7582d701
hansanga <cab08132@naver.com>
2025-06-16 00:02:13 +0900

    ✨ feat: 대화 중 날짜 바뀌면 새로운 날짜 출력

--- src/components/message/ChatRoom.tsx
+++ src/components/message/ChatRoom.tsx
@@ -29,6 +29,14 @@ export default function ChatRoom({
     setMessages((prev) => [...prev, msg]);
   };
 
+  const updateSeen = () => {
+    setMessages((prev) =>
+      prev.map((msg) =>
+        msg.sender_id === authId ? { ...msg, seen: true } : msg
+      )
+    );
+  };
+
   // 메세지 불러오기
   useEffect(() => {
     const loadMessages = async () => {
@@ -47,13 +55,15 @@ export default function ChatRoom({
     let channel: RealtimeChannel | null = null;
 
     const subscribe = async () => {
-      const res = await subscribeToMessages(chatRoomId, (msg: Message) => {
-        if (msg.sender_id === authId) return;
-        setMessages((prev) => [...prev, msg]);
-        console.log("authId", authId);
-        console.log("msg.sender_id", msg.sender_id);
-        console.log("equals?", String(msg.sender_id) === String(authId));
-      });
+      const res = await subscribeToMessages(
+        chatRoomId,
+        (msg: Message) => {
+          if (msg.sender_id === authId) return;
+          setMessages((prev) => [...prev, msg]);
+        },
+        updateSeen
+      );
+
       channel = res ?? null;
     };
 
@@ -64,12 +74,12 @@ export default function ChatRoom({
         channel.unsubscribe();
       }
     };
-  }, [chatRoomId]);
+  });
 
   // 메세지 오면 채팅창 맨아래로 이동
   useEffect(() => {
     if (messageEndRef.current) {
-      messageEndRef.current.scrollIntoView({ behavior: "auto" });
+      messageEndRef.current.scrollIntoView({ behavior: "smooth" });
     }
   }, [messages]);
 
@@ -96,47 +106,56 @@ export default function ChatRoom({
         </div>
         <hr className="mx-5 my-3 border-[var(--gray-500)] dark:bg-[var(--dark-bg-primary)]" />
         <div className="h-full overflow-y-scroll flex flex-col my-4">
-          <div className="flex items-center justify-center">
-            {messages.length > 0 && (
-              <span className="text-[var(--gray-500)] text-[10px]">
-                {dayjs(messages[0].created_at).format("YYYY년 MM월 DD일")}
-              </span>
-            )}
-          </div>
-          <div className="h-full w-full px-8 py-6">
+          <div className="h-full w-full px-8">
             {messages.map((message, index) => {
-              if (message.sender_id === authId) {
-                return (
-                  <div
-                    className="flex items-center gap-2.5 mb-4 justify-end w-full"
-                    key={index}
-                  >
-                    <span className="text-[10px] text-[#969696] self-end mb-1">
-                      {dayjs(message.created_at).format("HH:mm")}
-                    </span>
-                    <div className="max-w-96 px-4 py-3 bg-[#FFC7ED] text-black rounded-tl-2xl rounded-tr-2xl rounded-bl-2xl rounded-br-none text-[11px] whitespace-pre-line">
-                      <p>{message.message}</p>
-                    </div>
-                  </div>
-                );
-              } else {
-                return (
-                  <div className="flex gap-3.5 items-end" key={index}>
-                    <img
-                      src={chatUserProfile.main_image}
-                      className="w-[35px] h-[35px] rounded-full object-cover object-center"
-                    />
-                    <div className="flex items-end gap-2.5 mb-4" key={index}>
-                      <div className="max-w-96 px-4 py-3 bg-gray-200 text-black mr-auto text-left rounded-tl-2xl rounded-tr-2xl rounded-bl-none rounded-br-2xl text-[11px] whitespace-pre-line dark:bg-[var(--dark-bg-primary)] dark:text-[var(--dark-gray-100)]">
-                        <p>{message.message}</p>
-                      </div>
-                      <span className="text-[10px] text-[#969696]">
-                        {dayjs(message.created_at).format("HH:mm")}
+              const currentDate = dayjs(message.created_at).format(
+                "YYYY년 MM월 DD일"
+              );
+              const prevDate =
+                index > 0
+                  ? dayjs(messages[index - 1].created_at).format(
+                      "YYYY년 MM월 DD일"
+                    )
+                  : null;
+
+              const isNewDate = currentDate !== prevDate;
+
+              return (
+                <div key={index}>
+                  {isNewDate && (
+                    <div className="flex items-center justify-center">
+                      <span className="text-[var(--gray-500)] text-[10px] my-2">
+                        {currentDate}
                       </span>
                     </div>
-                  </div>
-                );
-              }
+                  )}
+                  {message.sender_id === authId ? (
+                    <div className="flex items-center gap-2.5 mb-4 justify-end w-full">
+                      <span className="text-[10px] text-[#969696] self-end mb-1">
+                        {dayjs(message.created_at).format("HH:mm")}
+                      </span>
+                      <div className="max-w-96 px-4 py-3 bg-[#FFC7ED] text-black rounded-tl-2xl rounded-tr-2xl rounded-bl-2xl rounded-br-none text-[11px] whitespace-pre-line">
+                        <p>{message.message}</p>
+                      </div>
+                    </div>
+                  ) : (
+                    <div className="flex gap-3.5 items-end">
+                      <img
+                        src={chatUserProfile.main_image}
+                        className="w-[35px] h-[35px] rounded-full object-cover object-center"
+                      />
+                      <div className="flex items-end gap-2.5 mb-4">
+                        <div className="max-w-96 px-4 py-3 bg-gray-200 text-black mr-auto text-left rounded-tl-2xl rounded-tr-2xl rounded-bl-none rounded-br-2xl text-[11px] whitespace-pre-line dark:bg-[var(--dark-bg-primary)] dark:text-[var(--dark-gray-100)]">
+                          <p>{message.message}</p>
+                        </div>
+                        <span className="text-[10px] text-[#969696]">
+                          {dayjs(message.created_at).format("HH:mm")}
+                        </span>
+                      </div>
+                    </div>
+                  )}
+                </div>
+              );
             })}
             <div ref={messageEndRef}></div>
           </div>
