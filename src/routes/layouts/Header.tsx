43c39ae6d1504adf77ed8b6bde3e2fe8e0faa285
JINJIY23 <dhkswksla22@naver.com>
2025-06-15 23:06:13 +0900

    ✨ Feature : 메인 운세 기능 변경
    메인 페이지 운세 기능 변경

--- src/routes/layouts/Header.tsx
+++ src/routes/layouts/Header.tsx
@@ -15,7 +15,7 @@ import {
 import HeaderModal from "../../components/modals/HeaderModal";
 import Notifications from "../../components/modals/Notifications";
 import { useDarkMode } from "../../hooks/useDarkMode";
-import { useAuthStore } from "../../stores/authStore";
+import { useAuthStore } from "../../stores/authstore";
 
 export default function Header() {
   const { isDark, toggleDarkMode } = useDarkMode();
