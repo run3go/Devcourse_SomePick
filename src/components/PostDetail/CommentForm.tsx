366d4c202026e4187087c6d4a3521cb0164f1426
hansanga <cab08132@naver.com>
2025-06-17 03:00:53 +0900

    ✨ feat: 비로그인시 댓글달기 막기

--- src/components/PostDetail/CommentForm.tsx
+++ src/components/PostDetail/CommentForm.tsx
@@ -4,6 +4,7 @@ import Icon from "../common/Icon";
 import { createComment, updateComment } from "../../apis/comment";
 // import Alert from "../common/Alert";
 import { showSuccessToast, showWarnToast } from "../common/ShowToast";
+import { useAuthStore } from "../../stores/authstore";
 import { notifyChildComment, notifyComment } from "../../apis/notification";
 
 interface CommentProps {
@@ -34,6 +35,7 @@ export default function CommentForm({
   parentAuthorId,
 }: CommentProps) {
   const [input, setInput] = useState(defaultValue);
+  const { session } = useAuthStore();
   // const [isAlertOpen, setIsAlertOpen] = useState(false);
 
   // 댓글 등록하기
@@ -64,7 +66,6 @@ export default function CommentForm({
             await notifyChildComment(parentAuthorId!, postId!);
             toggleReply?.();
           }
-
           // 댓글일때
         } else {
           if (postId !== null) await notifyComment(post.author.id, postId);
@@ -89,16 +90,24 @@ export default function CommentForm({
           <textarea
             value={input}
             onChange={(e) => setInput(e.target.value)}
-            placeholder="댓글을 작성해주세요."
+            placeholder={
+              session
+                ? "댓글을 작성해주세요."
+                : "로그인 후 댓글을 작성할 수 있어요."
+            }
             className={`bg-white rounded-2xl dark:bg-[var(--dark-bg-secondary)] dark:border-[var(--primary-pink)] dark:border dark:text-[var(--dark-gray-100)] ${
               isReply ? "w-[918px]" : "w-[980px]"
-            } resize-none px-[18px] py-[12px] text-[14px] mb-[10px] focus:outline-none focus:ring-2 focus:ring-[#FFC7ED]`}
+            } resize-none px-[18px] py-[12px] text-[14px] mb-[10px] focus:outline-none focus:ring-2 focus:ring-[#FFC7ED] ${
+              !session && "bg-[var(--gray-200)] text-gray-400"
+            }`}
+            disabled={!session}
           ></textarea>
         </div>
         <div className="flex w-full">
           <Button
             type="submit"
             className="ml-auto w-[98px] h-[38px] dark:text-[var(--dark-black)]"
+            disabled={!session}
           >
             등록
           </Button>
