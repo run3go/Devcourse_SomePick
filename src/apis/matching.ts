b6b966b9525d4b0874d8ff4ebc52e120c9399908
manto123123123 <lhw971103@gmail.com>
2025-06-16 15:29:19 +0900

    ♻️ :Refactor 소개팅페이지 로직 수정

--- src/apis/matching.ts
+++ src/apis/matching.ts
@@ -67,38 +67,13 @@ export const fetchMatchingUsers = async () => {
         `
       )
       .eq("is_matched", false)
-      .or(
-        `user_id.eq.${session.user.id}, matching_user_id.eq.${session.user.id}`
-      );
+      .or(`user_id.eq.${session.user.id}, matching_user_id.eq.${session.user.id}`);
     if (error) {
       console.log("유저 목록 조회 실패:", error.message);
       return;
     }
     console.log(profiles);
     return profiles;
-  } catch (e) {
-    console.error(e);
-  }
-};
-// 내가 하트를 보낸 상대인지 확인
-export const checkMatching = async (id: string) => {
-  try {
-    const {
-      data: { session },
-    } = await supabase.auth.getSession();
-    if (!session) return;
-    const { data: matching, error } = await supabase
-      .from("matchings")
-      .select("*")
-      .or(
-        `and(user_id.eq.${session.user.id},matching_user_id.eq.${id}),and(user_id.eq.${id},matching_user_id.eq.${session.user.id})`
-      )
-      .single();
-    if (error) {
-      console.log("유저 목록 조회 실패:", error.message);
-      return;
-    }
-    return matching;
   } catch (e) {
     console.error(e);
   }
@@ -121,9 +96,7 @@ export const fetchChatUsers = async () => {
         `
       )
       .eq("is_matched", true)
-      .or(
-        `user_id.eq.${session.user.id}, matching_user_id.eq.${session.user.id}`
-      );
+      .or(`user_id.eq.${session.user.id}, matching_user_id.eq.${session.user.id}`);
     if (error) {
       console.log("유저 목록 조회 실패:", error.message);
       return;
@@ -173,28 +146,6 @@ export const disconnectMatching = async (senderId: string) => {
       return;
     }
     console.log("매칭 거절 성공");
-  } catch (e) {
-    console.error(e);
-  }
-};
-
-export const deleteMatching = async () => {
-  try {
-    const {
-      data: { session },
-    } = await supabase.auth.getSession();
-    if (!session) return;
-    const { error } = await supabase
-      .from("matchings")
-      .delete()
-      .or(
-        `user_id.eq.${session.user.id}, matching_user_id.eq.${session.user.id}`
-      );
-    if (error) {
-      console.log("매칭 전부 삭제 실패:", error.message);
-      return;
-    }
-    console.log("매칭 전부 삭제 성공");
   } catch (e) {
     console.error(e);
   }
